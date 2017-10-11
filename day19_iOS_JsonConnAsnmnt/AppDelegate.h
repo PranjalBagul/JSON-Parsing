3530217fb78424e202ab62dc6a0b4478f8254b2a
Student P_08 <studentp_08@Student-P-08.local>
2017-10-11 15:38:36 +0530

    Initial Commit

---
+++ day19_iOS_JsonConnAsnmnt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day19_iOS_JsonConnAsnmnt
+//
+//  Created by Student P_08 on 11/10/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ day19_iOS_JsonConnAsnmnt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  day19_iOS_JsonConnAsnmnt
+//
+//  Created by Student P_08 on 11/10/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
